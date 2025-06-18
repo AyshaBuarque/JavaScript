40ab8f5e484a327bdcd1a9fa36a8023c27e511f1
AyshaBuarque <aysha.buarque.10@gmail.com>
2025-06-18 14:43:09 -0300

    Pronto

--- script.js
+++ script.js
@@ -32,16 +32,19 @@ function convertValues() {
 
 function changeCurrency () {
     const currencyName = document.getElementById ("currency-name")
-
+    const currencyImage = document.querySelector (".currency-img")
 
     if (currencySelect.value == "dolar") {
         currencyName.innerHTML = "Dólar americano"
+        currencyImage.src = "./assets/logo-Eua.png"
     }
     
      if (currencySelect.value == "euro") {
         currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/logo-Euro.png"
     }
-   
+    
+    convertValues()
 }
 
 currencySelect.addEventListener("change", changeCurrency)
